8afb34a26c8724babab9f469b3fdf189c01fa5cf
HunterAdv <gustavopaulody@gmail.com>
2023-02-04 13:11:57 -0300

    Finalizada tela de login

--- login.php
+++ login.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;900&display=swap" rel="stylesheet">
     <title>Mark 2 do</title>
 </head>
 <?php
-require_once("footer.php");
-
-session_start();
 
 if (isset($_SESSION['userName']))
     header("Location: index.php");
@@ -19,11 +19,19 @@ if (isset($_SESSION['userName']))
 
 <body>
     <section class="login">
+        <h1 class="title-app">Mark 2 do</h1>
         <div class="div-form-blue">
             <div class="div-form-white">
                 <form class="form" action="doLogin.php">
-                    <label for="inputPassword5" class="form-label">Senha</label>
-                    <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Senha</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
 
                     <div class="buttons">
                         <button type="button" class="btn btn-primary btn-lg">Login</button>
@@ -36,3 +44,7 @@ if (isset($_SESSION['userName']))
 </body>
 
 </html>
+
+<?php
+require_once("footer.php");
+?>
